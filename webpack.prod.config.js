e611af3cb71fa18506befd1f08427a57006d0293
mikaelss <mikaelrinosolstad@gmail.com>
2017-08-22 23:00:08 +0200

    node_env

--- webpack.prod.config.js
+++ webpack.prod.config.js
@@ -31,7 +31,7 @@ module.exports = {
   plugins: [
     new webpack.DefinePlugin({
       'process.env': {
-        'NODE_ENV': JSON.stringify('development')
+        'NODE_ENV': JSON.stringify('production')
       }
     }),
     new ExtractTextPlugin("styles.css"),
